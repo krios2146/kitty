3accfbcbc9ea6322352f195919cd33015f5bc14a
Vladimir Kidyaev <krios2146@gmail.com>
2024-07-16 06:23:34 +0700

    feat(nvim): remove icon between sections in lualine

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -3,6 +3,10 @@ return {
   {
     'nvim-lualine/lualine.nvim',
     opts = {
+      options = {
+        component_separators = { left = '', right = '' },
+        section_separators = { left = '', right = '' },
+      },
       sections = {
         lualine_x = {
           { 'datetime', style = '%H:%M:%S' },
